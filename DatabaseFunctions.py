3ade36d791bf9d57959139d46f61c7eefa4790ef
Ge0rgeM <g_modebadze3@cu.edu.ge>
2024-04-03 00:15:23 +0400

    Trying to solve streamlit

--- DatabaseFunctions.py
+++ DatabaseFunctions.py
@@ -1,20 +1,20 @@
-import mysql.connector
+# import mysql.connector
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-def print_database():
-    try:
-        db = mysql.connector.connect(
-            host=os.environ.get('host'),
-            user=os.environ.get('user'),
-            password=os.environ.get('password'),
-            database=os.environ.get('database')
-        )
-        if db.is_connected():
-            print("Connected to MySQL database")
-    except mysql.connector.Error as e:
-        print(f"Error connecting to MySQL database: {e}")
-
-print_database()
+# def print_database():
+#     try:
+#         db = mysql.connector.connect(
+#             host=os.environ.get('host'),
+#             user=os.environ.get('user'),
+#             password=os.environ.get('password'),
+#             database=os.environ.get('database')
+#         )
+#         if db.is_connected():
+#             print("Connected to MySQL database")
+#     except mysql.connector.Error as e:
+#         print(f"Error connecting to MySQL database: {e}")
+print(os.environ.get('host'))
+# print_database()
